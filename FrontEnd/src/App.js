a6fd9471d713eab3e7a031bffdb8b702cc4dce3c
todo-list <dinhthanhtien2k2@gmail.com>
2022-07-14 10:58:21 +0700

    Branch of ThanhTien

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Branch of ThanhTien
         </a>
       </header>
     </div>

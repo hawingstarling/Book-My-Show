7b2d3c5b1a2aa733c4c4947c37a87af9b4f2602f
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-17 17:28:58 +0700

    Update MiddleWare VerifyAdmin 17/7/2022

--- Backend/src/MiddleWare/Admin.js
+++ Backend/src/MiddleWare/Admin.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { ACCESS_SEC } = require("../util/Key/keys");
+const { MSG } = require("../util/MessageToClient");
 
 const VerifyAdmin = function (req, res, next) {
   const authorizationHeader = req.headers["authorization"];
@@ -8,7 +9,8 @@ const VerifyAdmin = function (req, res, next) {
   jwt.verify(token, ACCESS_SEC, (err, data) => {
     console.log(data,9)
     if (err) return res.sendStatus(403);
-    next();
+    if(data.isAdmin) next()
+    else return res.status(404).json(MSG("You're not permission to do this",404))
   });
   //   next();
 };

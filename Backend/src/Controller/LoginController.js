655b8b4e9704e77b67cf94d04da08f78d91bd9a2
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-17 17:19:19 +0700

    Fixe Login Route 17/7/2022

--- Backend/src/Controller/LoginController.js
+++ Backend/src/Controller/LoginController.js
@@ -2,7 +2,12 @@ const jwt = require("jsonwebtoken");
 const User = require("../Model/User");
 const CryptoJS = require("crypto-js");
 
-const { ACCESS_SEC, HASH_SEC, TIME_EXPIRED, REFRESH_SEC } = require("../util/Key/keys");
+const {
+  ACCESS_SEC,
+  HASH_SEC,
+  TIME_EXPIRED,
+  REFRESH_SEC,
+} = require("../util/Key/keys");
 const Keys = require("../util/Key/keys");
 const LoginController = {
   Login: async function (req, res) {
@@ -16,19 +21,26 @@ const LoginController = {
         let bytes = CryptoJS.AES.decrypt(UserFromDB.password, HASH_SEC);
         let decryptedEmail = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
         if (decryptedEmail == dataFormClient.password) {
-          const { password,...others } = UserFromDB._doc;
-          const accessToken = jwt.sign(others, Keys.ACCESS_SEC, {
+          const { password, refreshToken, accessToken, ...others } =
+            UserFromDB._doc;
+          const AccessToken = jwt.sign(others, Keys.ACCESS_SEC, {
             expiresIn: TIME_EXPIRED,
           });
-          const RefreshToken = jwt.sign(others,REFRESH_SEC)
-          others.refreshToken.push(RefreshToken);
-          others.accessToken.push(accessToken);
-          await User.findByIdAndUpdate(others._id,others);
+          const RefreshToken = jwt.sign(others, REFRESH_SEC);
+          refreshToken.push(RefreshToken);
+          accessToken.push(AccessToken);
+          others.refreshToken = refreshToken;
+          others.accessToken = accessToken;
+          await User.findByIdAndUpdate(others._id, others);
           console.log("Coas");
           res.json({
             msg: "Success",
             statuse: 200,
-            data: others,
+            data: {
+              ...others,
+              accessToken: AccessToken,
+              refreshToken: RefreshToken,
+            },
           });
         } else {
           res.json({
